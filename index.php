9dc6eecd2efbec43d92bff31eccb8b2a9f46af15
Lorens <168178279+Renzify@users.noreply.github.com>
2026-02-21 12:22:32 +0800

    operators

--- index.php
+++ index.php
@@ -6,27 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-     <main>
-        <form action="./includes/formhandler.php" method="post">
-            <label for="firstname">First Name:</label>
-            <input type="text" id="firstname" name="firstname" placeholder="First Name...">
-            <br>
+    <h1>PHP Operators: </h1>
+    <!-- Pretty standard stuffs -->
+    <ul>
+        <li>String Operators</li>
+        <li>Arithmetic Operators</li>
+        <li>Assignment Operators</li>
+        <li>Comparison Operators</li>
+        <li>Logical Operators</li>
+        <li>Icrementing and Decrementing Operators</li>
+    </ul>
 
-            <label for="lastname">Last Name:</label>
-            <input type="text" id="lastname" name="lastname" placeholder="Last Name...">
-            <br>
-
-            <label for="favoritepet">Favorite Pet: </label>
-            <select name="favoritepet" id="favoritepet">
-                <option value="none">None</option>
-                <option value="dog">Dog</option>
-                <option value="cat">Cat</option>
-                <option value="bird">Bird</option>
-            </select>
-            <br>
-
-            <button type="submit">Submit</button>
-        </form>
-     </main>
 </body>
 </html>
