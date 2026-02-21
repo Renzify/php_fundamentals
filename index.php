16c17c2b74ce70a1b975edf500f8360ca1d76513
Lorens <168178279+Renzify@users.noreply.github.com>
2026-02-21 12:52:52 +0800

    conditions & control structures

--- index.php
+++ index.php
@@ -6,16 +6,46 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>PHP Operators: </h1>
-    <!-- Pretty standard stuffs -->
-    <ul>
-        <li>String Operators</li>
-        <li>Arithmetic Operators</li>
-        <li>Assignment Operators</li>
-        <li>Comparison Operators</li>
-        <li>Logical Operators</li>
-        <li>Icrementing and Decrementing Operators</li>
-    </ul>
+    <?php
+
+    $bool = true;
+    $a = 1;
+    $b = 4;
+
+    // if elseif else
+    if ($a < $b && !$bool){
+        echo "Condition 1 is true";
+    }
+    else if ($a > $b && $bool){
+        echo "Condition 2 is true";
+    } 
+    else {
+        echo "Else";
+    }
+
+    // switch case
+    switch ($a){
+        case 1:
+            echo "1 case";
+            break;
+        
+        case 67:
+            echo "67 case";
+            break;
+        
+        default:
+            echo "default";
+    }
+
+    // match
+    $result = match ($a) {
+            1, 3, 5 => "Variable a is odd",
+            2 => "Variable a is 2",
+            default => "No match",
+    };
+
+    echo $result;
+    ?>
 
 </body>
 </html>
