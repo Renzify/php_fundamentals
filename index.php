e50db416011baf272a68adc3aa0a5624d0dca8d0
Lorens <168178279+Renzify@users.noreply.github.com>
2026-02-21 14:12:42 +0800

    initial php calculator

--- index.php
+++ index.php
@@ -3,49 +3,71 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Calculator</title>
 </head>
 <body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <input type="number" name="num1" placeholder="1st Number">
+        <select name="operator">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="text" name="num2" placeholder="2nd Number">
+    <button>Caculate</button>
+    </form>
+
     <?php
+        // Check Request
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        // Read/Grab input safely
+            $num1 = filter_input(INPUT_POST, "num1", FILTER_SANITIZE_NUMBER_FLOAT);
+            $num2 = filter_input(INPUT_POST, "num2", FILTER_SANITIZE_NUMBER_FLOAT);
+            $operator = filter_input(INPUT_POST, "operator", FILTER_SANITIZE_SPECIAL_CHARS);
 
-    $bool = true;
-    $a = 1;
-    $b = 4;
+        // Validate / Error Handling
+            $errors = false;
 
-    // if elseif else
-    if ($a < $b && !$bool){
-        echo "Condition 1 is true";
-    }
-    else if ($a > $b && $bool){
-        echo "Condition 2 is true";
-    } 
-    else {
-        echo "Else";
-    }
+            // Check if empty
+            if (empty($num1) || empty($num2) || empty($operator)) {
+                echo "<p>Please fill in all fields</p>";
+                $errors = true;
+            }
 
-    // switch case
-    switch ($a){
-        case 1:
-            echo "1 case";
-            break;
-        
-        case 67:
-            echo "67 case";
-            break;
-        
-        default:
-            echo "default";
-    }
+            // Check if valid inputs
+            if (!is_numeric($num1) || !is_numeric($num2)) {
+                echo "<p>Please input a number</p>";
+                $errors = true;
+            }
 
-    // match
-    $result = match ($a) {
-            1, 3, 5 => "Variable a is odd",
-            2 => "Variable a is 2",
-            default => "No match",
-    };
+            // Process
+            if (!$errors){
+                $value = 0;
+                
+                switch ($operator){
+                    case "add":
+                        $value = $num1 + $num2;
+                      break;
 
-    echo $result;
+                    case "subtract":
+                        $value = $num1 - $num2;
+                        break;
+                    
+                    case "multiply":
+                        $value = $num1 * $num2;
+                        break;
+
+                    case "divide":
+                        $value = $num1 / $num2;
+                        break;
+                    default:
+                        echo "<p>Internal Server Error</p>";
+                }
+            //Output
+                echo "<p>Result = " . $value . "</p>";
+            }
+        }
     ?>
-
 </body>
 </html>
